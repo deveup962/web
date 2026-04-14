700a1bb7e458d391c3d481d611d3287421189a70
devups962 <eup_dev_1@proton.me>
2026-04-14 19:38:33 +0200

    .

--- script.js
+++ script.js
@@ -27,3 +27,93 @@
             signupModal.addEventListener('shown.bs.modal', autoFillCountry);
         }
     });
+
+
+    const depositInput = document.getElementById('deposit');
+    const daysSlider = document.getElementById('daysSlider');
+    const daysText = document.getElementById('daysText');
+    const finalBalance = document.getElementById('finalBalance');
+
+  
+    const targets = {
+        10: 408, 20: 664, 30: 1081, 40: 1760,
+        50: 2867, 60: 4670, 70: 7607, 80: 12391,
+        90: 20183, 100: 32876
+    };
+
+    function calculate() {
+        let deposit = parseFloat(depositInput.value) || 250;
+        let days = parseInt(daysSlider.value) || 1;
+
+        let result;
+
+       
+        if (targets[days]) {
+            result = targets[days];
+        } else {
+            
+            const dailyRate = 0.050027;
+            result = deposit * Math.pow(1 + dailyRate, days);
+            result = Math.round(result);
+        }
+
+        
+        if (result < deposit) result = deposit;
+
+        finalBalance.textContent = result.toLocaleString('en-GB') + " £";
+        daysText.textContent = days + (days === 1 ? " day" : " days");
+    }
+
+   
+    depositInput.addEventListener('input', calculate);
+    daysSlider.addEventListener('input', calculate);
+
+   
+    calculate();
+
+
+function toggleFaq(btn) {
+  const answer = btn.nextElementSibling;
+  const icon = btn.querySelector('.faq-icon');
+  
+  if (answer.style.display === "none" || answer.style.display === "") {
+    answer.style.display = "block";
+    icon.textContent = "▲";
+    icon.style.color = "#E85B77";
+  } else {
+    answer.style.display = "none";
+    icon.textContent = "▼";
+    icon.style.color = "";
+  }
+}
+
+
+
+ function switchToSignup() {
+    const signinModal = bootstrap.Modal.getInstance(document.getElementById('signinModal'));
+    signinModal.hide();
+    
+    setTimeout(() => {
+      new bootstrap.Modal(document.getElementById('signupModal')).show();
+    }, 500);
+  }
+
+
+  function showForgotPassword() {
+    const signinModal = bootstrap.Modal.getInstance(document.getElementById('signinModal'));
+    if (signinModal) signinModal.hide();
+    
+    setTimeout(() => {
+      new bootstrap.Modal(document.getElementById('forgotPasswordModal')).show();
+    }, 400);
+  }
+
+
+  function backToSignIn() {
+    const forgotModal = bootstrap.Modal.getInstance(document.getElementById('forgotPasswordModal'));
+    if (forgotModal) forgotModal.hide();
+    
+    setTimeout(() => {
+      new bootstrap.Modal(document.getElementById('signinModal')).show();
+    }, 400);
+  }
